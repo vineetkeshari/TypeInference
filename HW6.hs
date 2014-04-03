dac08f94047140cd799ca48217994dc8d1312ca7
Vineet Keshari <vkeshari@cs.utexas.edu>
2014-04-03 15:58:58 -0500

    Reset show exp

--- HW6.hs
+++ HW6.hs
@@ -29,7 +29,7 @@ data Exp = Literal   Value
          | Declare   String Exp Exp
          | Function  String Exp      -- new
          | Call      Exp Exp         -- changed
-  deriving (Eq,Show)
+  deriving (Eq)
   
 type Env = [(String, Value)]
 
@@ -143,7 +143,6 @@ substExp x e (Function y e') = if x == y
                                     else (Function y (substExp x e e'))
 substExp x e (Call e1 e2) = Call (substExp x e e1) (substExp x e e2)
 
-{-
 -- Code to display expressions
 instance Show Exp where
   show e = "[" ++ showExp 0 e ++ "]"
@@ -172,7 +171,7 @@ showBinary outer inner a op b =
       where result = showExp inner a ++ " " ++ op ++ " " ++ showExp inner b
       
 paren x = "(" ++ x ++ ")"
--}
+
 data Type = TInt
           | TBool
           | TVar String
